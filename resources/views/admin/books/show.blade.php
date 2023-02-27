51b9afdb6c05ebab6868656a79289b90d7283ddd
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-27 16:47:49 +0100

    admin show.blade commit

--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -1,3 +1,22 @@
 @extends('layouts.app')
 @section('content')
+<div class="container mt-5">
+    <div class="card p-3">
+        <h2>{{$book->title}}</h2>
+        {{-- <img src="{{asset('storage/'. $book->cover)}}" alt="placeholder"> --}}
+        <pre>{{$book->author}}</pre>
+        <pre>{{$book->genre}}</pre>
+        <pre>{{$book->release_date}}</pre>
+        <pre>{{$book->ISBN}}</pre>
+        <p>{{$book->description}}</p>
+    </div>
+
+
+    
+    
+</div>
+
+
+
+
 @endsection
